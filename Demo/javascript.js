fa04c3dd05e3d814acb8a32a9c798818b6b0ae9a
oshanDM <it20070076@my.sliit.lk>
2024-07-17 00:03:04 +0530

    learnjs

---
+++ Demo/javascript.js
@@ -0,0 +1,18 @@
+
+let btn = document.getElementById('btn_click');
+
+// btn.addEventListener('click', btnClickEvent);
+
+
+// function btnClickEvent(){
+//     alert("Hello Javascript!");
+// }
+
+btn.addEventListener('click', function btnClickEvent(){
+    alert("Hello Javascript!");
+});
+
+
+
+
+
